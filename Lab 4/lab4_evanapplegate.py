bcdc463f8add692ed3cbeb8a39cff3b2eb1017b0
evanapplegate <evandapplegate@gmail.com>
2015-10-08 18:37:22 -0500

    Lab 4 looks good, added comments

--- Lab 4/lab4_evanapplegate.py
+++ Lab 4/lab4_evanapplegate.py
@@ -1,5 +1,4 @@
 # encoding: utf-8
-import sys
 
 # Question 1: this function takes a tuple of three values and converts them to decimal degrees
 def DMS_to_DD(degrees, minutes, seconds):   
@@ -17,16 +16,14 @@ def DMS_to_DD(degrees, minutes, seconds):
         DD = float(degrees) + (float(minutes*-1)/60) + (float(seconds*-1)/3600)
         return DD
 
-# this is just a test statement
-# print DMS_to_DD(-30,30,00)
+# this is a test statement
+# print DMS_to_DD(-30,30,0)
 
 # Question 2: this function takes a single value and converts it to decimal degrees
 def DD_to_DMS(decimal_degrees):
     
     # if the degree value is positive, then run the DMS > DD formula as below.
-    # values converted to integers per the formula
-        # but M has to be converted back into a float because otherwise any decimal
-        # values will be dropped to zero
+    # M (the minutes value) has to be converted back into a float because otherwise any decimal values will be dropped to zero
     # no error handling if someone tries to put in an illegal coordinate value, but there should be
 
     if decimal_degrees >= 0:
@@ -43,28 +40,44 @@ def DD_to_DMS(decimal_degrees):
         S = int((decimal_degrees - D - (float(M)/60))*3600)
         return D, abs(M), abs(S)
 
-# this is just a test statement    
-# print DD_to_DMS(102.263888889)
+# this is a test statement
+# print DD_to_DMS(25.51251)
 
+# This asks the user to type in a coordinate
 user_input = raw_input("Enter a latitude or longitude value in DMS or DD format. Just numbers, commas between values, \
 and no spaces: ")
 
+# This converts the input string to a list of values 
 user_input_evaluation = user_input.split(",")
 
+# If the input string has only 1 value, i.e. it's a list with 1 item, then this assumes
+# that the input is a decimal degree coordinate
 if len(user_input_evaluation) == 1:
     DD_value_to_convert = float(user_input_evaluation[0])
     DD_to_DMS_function_output = DD_to_DMS(DD_value_to_convert)
+    
+    # the return of the DD_to_DMS function is a list with 3 values. Here, the D, M, and S
+    # values are split into separate variables; the D is the first item in the list,
+    # M is the second value, and S is the third value.
     D_value = DD_to_DMS_function_output[0]
     M_value = DD_to_DMS_function_output[1]
     S_value = DD_to_DMS_function_output[2]
+    
+    # This prints the result with string concatentation.
     print "The input value is in DD form."
-    print "Its DMS form is "+ str(D_value)+"º " + str(M_value)+"' "+ str(S_value)+"\""
+    print "Converted to DMS, it would read "+ str(D_value)+"º " + str(M_value)+"' "+ str(S_value)+"\""
 
+# If the input string has 3 values, i.e. it's a list with 3 items, then this assumes
+# that the input is a DMS coordinate
 if len(user_input_evaluation) == 3:
+    
+    # the three input values are converted from strings to floats before being sent
+    # to the DMS_to_DD function
     degrees = float(user_input_evaluation[0])
     minutes = float(user_input_evaluation[1])
     seconds = float(user_input_evaluation[2])
-    #DMS_value_to_convert = str(degrees) minutes, seconds 
     DMS_to_DD_function_output = DMS_to_DD(degrees, minutes, seconds)
+    
+    # This prints the result with string concatentation.
     print "The input value is in DMS form."
-    print "Its DD form is "+ str(DMS_to_DD_function_output)+"º"
+    print "Converted to DD, it would read "+ str(DMS_to_DD_function_output)+"º"
