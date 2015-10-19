d27fb4b6dffe8bb829dd85bb00d323441874590e
evanapplegate <evandapplegate@gmail.com>
2015-10-19 17:11:16 -0500

    Sorting was kicking my ass, but figured out the loop!

--- Topic 7 - containers/7-6.py
+++ Topic 7 - containers/7-6.py
@@ -1,2 +1,12 @@
-tlist = list(raw_input('Enter telephone number >'))
-         print tlist
+rects = [(5,2), (7,13), (9,4), (17,4)]
+
+def calculate_areas(a):
+    area_list = []
+    for a, b in rects:
+        area_of_rectangle = a*b
+        area_list.append(area_of_rectangle)
+    return area_list
+
+areas_to_sort = calculate_areas(rects)
+areas_to_sort.sort(reverse=True)
+print areas_to_sort
