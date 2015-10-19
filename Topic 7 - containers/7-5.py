d7fb6a88964da0e2e4b7a8dc69d3dea1581c9265
evanapplegate <evandapplegate@gmail.com>
2015-10-19 16:14:46 -0500

    Exercise 7.5's listed solution blows

--- Topic 7 - containers/7-5.py
+++ Topic 7 - containers/7-5.py
@@ -1,2 +1,10 @@
-tlist = list(raw_input('Enter telephone number >'))
-         print tlist
+telephone_number_list = list(raw_input('Enter telephone number >'))
+good_telephone_number = []
+
+for i in range(len(telephone_number_list)):
+    if telephone_number_list[i].isdigit():
+        good_telephone_number.append(telephone_number_list[i])
+        
+final_number = "".join(good_telephone_number)
+
+print "the phone number stripped of non-digits is:", final_number
